28769ecd9882d192591a443e6b90609fcf0acb47
Tristan.S-C <tri-23-sc@hotmail.ca>
2023-10-16 22:12:28 -0400

    - Ajout de Cours 1 exercise perimetre

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 import static Class1.HelloWorld.runHello;
+import static Class1.PerimeterAssignmentRunner.runPerimeterAssignmentRunner;
 
 public class Main {
     public static void main (String[] args) {
         try {
 
-            runHello(new String[0]);
+            //runHello(new String[0]);
+            runPerimeterAssignmentRunner(new String[0]);
 
         } catch (Exception e) {
 
