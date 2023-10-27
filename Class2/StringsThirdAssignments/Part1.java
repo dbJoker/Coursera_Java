f7b9ad73d97047985356224be2d8e319bbdbd8d9
Tristan.S-C <tri-23-sc@hotmail.ca>
2023-10-27 18:48:23 -0400

    - C2 String Third Assignement DNA Part 1

---
+++ Class2/StringsThirdAssignments/Part1.java
@@ -0,0 +1,151 @@
+package Class2.StringsThirdAssignments;
+
+import edu.duke.StorageResource;
+
+public class Part1 {
+
+    public static void runStringsThirdAssignmentsPart1 (String[] args) {
+        Class2.StringsThirdAssignments.Part1 run = new Class2.StringsThirdAssignments.Part1();
+        run.testFindStopCodon();
+        run.testFindGene();
+        run.printAllGenes("GATGTCACTGCTCCATTGATTAAGCAAGTCTGACACACATGTAGCTAAGCTGTGAGTTAATGTATGCAGCGATCTAACACCCACGCC");
+        run.testGetAllGenes("GATGTCACTGCTCCATTGATTAAGCAAGTCTGACACACATGTAGCTAAGCTGTGAGTTAATGTATGCAGCGATCTAACACCCACGCC");
+    }
+
+    public int findStopCodon (String dna, int startIndex,String stopCodon) {
+
+        int stopCodonIndex = dna.toUpperCase().indexOf(stopCodon.toUpperCase(), startIndex + 3);
+        if (stopCodonIndex == -1) {
+            return dna.length();
+        }
+        if ((stopCodonIndex - startIndex) % 3 != 0) {
+            return dna.length();
+        }
+
+        return stopCodonIndex;
+    }
+
+
+    public String findGene(String dna) {
+
+        int startCodonIndex = dna.toUpperCase().indexOf("ATG");
+        if (startCodonIndex == -1) {
+            return "";
+        }
+
+        int indexStopCondonTAA = findStopCodon(dna, startCodonIndex, "TAA");
+        int indexStopCondonTAG = findStopCodon(dna, startCodonIndex, "TAG");
+        int indexStopCondonTGA = findStopCodon(dna, startCodonIndex, "TGA");
+
+        int indexStopMin = 0;
+
+        if (indexStopCondonTAA < indexStopCondonTAG) {
+            indexStopMin = indexStopCondonTAA;
+        } else {
+            indexStopMin = indexStopCondonTAG;
+        }
+
+        if (indexStopCondonTGA < indexStopMin ) {
+            indexStopMin = indexStopCondonTGA;
+        }
+
+        if(indexStopMin != dna.length()) {
+            return dna.substring(startCodonIndex, indexStopMin + 3);
+        } else {
+            return "";
+        }
+    }
+
+    public StorageResource getAllGenes(String dna) {
+
+        StorageResource geneList = new StorageResource();
+        String currentDna = dna;
+        while (true) {
+
+            String gene = findGene(currentDna);
+
+            if (!gene.isEmpty()) {
+                geneList.add(gene);
+                currentDna = currentDna.substring(currentDna.indexOf(gene) + gene.length());
+            } else {
+                break;
+            }
+
+        }
+        return geneList;
+    }
+
+    public void printAllGenes(String dna) {
+
+        System.out.println("dna : " + dna);
+        System.out.println("Gene found : ");
+        String currentDna = dna;
+        while (true) {
+
+            String gene = findGene(currentDna);
+
+            if (!gene.isEmpty()) {
+                System.out.println(gene);
+                currentDna = currentDna.substring(currentDna.indexOf(gene) + gene.length());
+            } else {
+                break;
+            }
+
+        }
+    }
+
+    public void testFindStopCodon() {
+
+        System.out.println("Test Starting : ");
+
+        System.out.print("Expected : 9 ");
+        System.out.println("Result : " +
+                findStopCodon("ATGTGGTTGTAAGTGTAATG", 0, "TAA")
+        );
+
+        System.out.print("Expected : 12 ");
+        System.out.println("Result : " +
+                findStopCodon("TTGATGTGGTTGTGAGTGTAA", 0, "TGA")
+        );
+
+        System.out.print("Expected : 17 ");
+        System.out.println("Result : " +
+                findStopCodon("ATGTGGTTGTGAGTGTA", 0, "TAA")
+        );
+
+
+    }
+
+    public void testFindGene() {
+
+        String dnaWithNoStartCodon = "TGGTGTGGTTGTAAGTGTAATG";
+        String dnaWithOneStopCodon = "GATGTGGTATGGGTGAT";
+        String dnaWithMultipleStopCodon = "GATGTGGTTGTAAGTGTAGTG";
+        String dnaWithNoValidStopCodon = "GATGTGGTTTAAGTGTAGTG";
+
+        System.out.println("DNA : " + dnaWithNoStartCodon);
+        System.out.println("Gene : " + findGene(dnaWithNoStartCodon));
+
+        System.out.println("DNA : " + dnaWithOneStopCodon);
+        System.out.println("Gene : " + findGene(dnaWithOneStopCodon));
+
+        System.out.println("DNA : " + dnaWithMultipleStopCodon);
+        System.out.println("Gene : " + findGene(dnaWithMultipleStopCodon));
+
+        System.out.println("DNA : " + dnaWithNoValidStopCodon);
+        System.out.println("Gene : " + findGene(dnaWithNoValidStopCodon));
+    }
+
+    public void testGetAllGenes(String dna) {
+        System.out.println("********");
+        System.out.println("Testing getAllGenes : ");
+
+        StorageResource genes = getAllGenes(dna);
+        for (String gene : genes.data()) {
+            System.out.println(gene);
+        }
+
+
+    }
+
+}
--- Main.java
+++ Main.java
@@ -8,15 +8,18 @@ import static Class2.StringsFirstAssignments.Part4.runStringFirstAssignmentPart4
 import static Class2.StringsSecondAssignments.Part1.runStringSecondAssignmentPart1;
 import static Class2.StringsSecondAssignments.Part2.runStringSecondAssignmentPart2;
 import static Class2.StringsSecondAssignments.Part3.runStringSecondAssignmentPart3;
+import static Class2.StringsThirdAssignments.Part1.runStringsThirdAssignmentsPart1;
 
 public class Main {
     public static void main (String[] args) {
         try {
 
+            //*****Class1*****
             //runHello(new String[0]);
 
             //runPerimeterAssignmentRunner(new String[0]);
 
+            //*****Class2*****
             //runStringFirstAssignmentPart1(new String[0]);
             //runStringFirstAssignmentPart2(new String[0]);
             //runStringFirstAssignmentPart3(new String[0]);
@@ -26,7 +29,12 @@ public class Main {
             //runStringSecondAssignmentPart2(new String[0]);
             //runStringSecondAssignmentPart3(new String[0]);
 
-            runDebuffing(new String[0]);
+            //runDebuffing(new String[0]);
+
+            runStringsThirdAssignmentsPart1(new String[0]);
+
+
+            //*****Class3*****
 
         } catch (Exception e) {
             e.printStackTrace();
